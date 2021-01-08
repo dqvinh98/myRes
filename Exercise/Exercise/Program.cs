5a89454ba96011ea1d6e650932c8cd852a66124c
dqvinh98 <dqvinh98@gmail.com>
2021-01-08 11:35:52 +0700

    Update Shape exercise

--- Exercise/Exercise/Program.cs
+++ Exercise/Exercise/Program.cs
@@ -10,6 +10,49 @@ namespace Exercise
     {
         static void Main(string[] args)
         {
+            float a, b, c;
+            string t;
+            Shape s;
+            do
+            {
+                Console.WriteLine("press 0(exit), 1(Rectangle), 2(Square), 3(Triangle), 4(Circle)");
+                t = Console.ReadLine();
+                switch (t)
+                {
+                    case "1":
+                        Console.Write("a=");
+                        a = float.Parse(Console.ReadLine());
+                        Console.Write("b=");
+                        b = float.Parse(Console.ReadLine());
+                        s = new Rectangle(a, b);
+                        Console.WriteLine("perimeter: " + s.Perimeter());
+                        break;
+                    case "2":
+                        Console.Write("a=");
+                        a = float.Parse(Console.ReadLine());
+                        s = new Square(a);
+                        Console.WriteLine("perimeter: " + s.Perimeter());
+                        break;
+                    case "3":
+                        Console.Write("a=");
+                        a = float.Parse(Console.ReadLine());
+                        Console.Write("b=");
+                        b = float.Parse(Console.ReadLine());
+                        Console.Write("c=");
+                        c = float.Parse(Console.ReadLine());
+                        s = new Triangle(a, b, c);
+                        Console.WriteLine("perimeter: " + s.Perimeter());
+                        break;
+                    case "4":
+                        Console.Write("a=");
+                        a = float.Parse(Console.ReadLine());
+                        s = new Circle(a);
+                        Console.WriteLine("perimeter: " + s.Perimeter());
+                        break;
+                }
+                
+            } while (t != "0");
         }
+
     }
 }
---
+++ Exercise/Exercise/Shape.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    interface Shape
+    {
+        float Perimeter();
+    }
+
+    class Rectangle : Shape
+    {
+        private float a;
+        private float b;
+
+        public Rectangle(float a, float b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public float Perimeter()
+        {
+            return (a + b) * 2;
+        }
+    }
+
+    class Square: Shape
+    {
+        private float a;
+        public Square(float a)
+        {
+            this.a = a;
+        }
+
+        public float Perimeter()
+        {
+            return a * 4;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        private float a;
+        private float b;
+        private float c;
+        public Triangle(float a, float b, float c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public float Perimeter()
+        {
+            return a + b + c;
+        }
+    }
+
+    class Circle : Shape
+    {
+        private float a;
+        public Circle(float a)
+        {
+            this.a = a;
+        }
+
+        public float Perimeter()
+        {
+            return a * 2 * 3.14f;
+        }
+    }
+}
